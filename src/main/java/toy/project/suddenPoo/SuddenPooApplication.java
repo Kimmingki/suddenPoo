af76b69f9ae71081eb31d1b5c180138d9d9b209b
Kimmingki <rlaalsrl9569@naver.com>
2024-08-21 13:51:22 +0900

    프로젝트 생성

---
+++ src/main/java/toy/project/suddenPoo/SuddenPooApplication.java
@@ -0,0 +1,13 @@
+package toy.project.suddenPoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuddenPooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuddenPooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toy/project/suddenPoo/SuddenPooApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.project.suddenPoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuddenPooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
