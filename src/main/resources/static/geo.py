1f095e823b6f4df5bd6ff956ddeb4f20f180f570
Kimmingki <rlaalsrl9569@naver.com>
2024-08-30 12:49:21 +0900

    화장실 주소 변환 코드 수정

--- src/main/resources/static/geo.py
+++ src/main/resources/static/geo.py
@@ -5,16 +5,21 @@ from tqdm import tqdm
 geo_local = Nominatim(user_agent='South Korea')
 
 def geocoding(address):
+	# 주소가 유효하지 않은 경우
+	if not address or pd.isna(address) or len(address.strip()) == 0:
+		return None
 	try:
 		geo = geo_local.geocode(address)
-		x_y = [geo.latitude, geo.longitude]
-		return x_y
+		if geo:
+			return [geo.latitude, geo.longitude]
+		else:
+			return None
 	except:
-		return [0, 0]
+		return None
 
 
 # 데이터 불러오기
-df = pd.read_csv('./toilet.csv', encoding='utf8')
+df = pd.read_csv('./toilet.csv', encoding='utf8', nrows=1000)
 address = df['소재지도로명주소']
 
 latitude = []
@@ -22,7 +27,15 @@ longitude = []
 
 # tdqm 적용 및 경도, 위도 출력
 for i in tqdm(address, desc="Processing address", unit="address"):
-	lat, lng = geocoding(i)
+	geocode_result = geocoding(i);
+
+	# 주소가 존재하지 않을 경우 WGS84 좌표 사용
+	if geocode_result:
+		lat, lng = geocode_result
+	else:
+		lat = df.loc[df['소재지도로명주소'] == i, 'WGS84위도'].values[0] if not pd.isna(df.loc[df['소재지도로명주소'] == i, 'WGS84위도'].values[0]) else 0
+		lng = df.loc[df['소재지도로명주소'] == i, 'WGS84경도'].values[0] if not pd.isna(df.loc[df['소재지도로명주소'] == i, 'WGS84경도'].values[0]) else 0
+
 	latitude.append(lat)
 	longitude.append(lng)
 
